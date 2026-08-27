65f5372c2703226cdf97b7192f9898658bb9bf1a
Aldo Borrero <82811+aldoborrero@users.noreply.github.com>
2026-08-27 13:33:06 +0200

    feat(nix): pcmflux manylinux wheel (F1 escape hatch)

---
+++ nix/packages/selkies-pcmflux-wheel/default.nix
@@ -0,0 +1,33 @@
+{ pkgs, ... }:
+pkgs.python3Packages.buildPythonPackage {
+  pname = "pcmflux";
+  version = "2.0.0";
+  format = "wheel";
+
+  src = pkgs.python3Packages.fetchPypi {
+    pname = "pcmflux";
+    version = "2.0.0";
+    format = "wheel";
+    dist = "cp314";
+    python = "cp314";
+    abi = "cp314";
+    platform = "manylinux_2_28_x86_64";
+    hash = "sha256-BQqMfXwJnuNjJ58IXKtl5fmJHcarlIGj7fZKkB0nY1I=";
+  };
+
+  nativeBuildInputs = [ pkgs.autoPatchelfHook ];
+
+  buildInputs = with pkgs; [
+    libpulseaudio
+    libopus
+    stdenv.cc.cc.lib
+    libx11
+    libxext
+    libice
+    libsm
+  ];
+
+  pythonImportsCheck = [ "pcmflux" ];
+
+  meta.description = "Selkies pcmflux (prebuilt manylinux wheel)";
+}
