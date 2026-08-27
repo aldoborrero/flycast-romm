1fd65653997210f03b85d14fb278c528b1ddecd4
Aldo Borrero <82811+aldoborrero@users.noreply.github.com>
2026-08-27 14:10:03 +0200

    feat(nix): pixelflux manylinux wheel (F1 escape hatch)

---
+++ nix/packages/selkies-pixelflux-wheel/default.nix
@@ -0,0 +1,34 @@
+{ pkgs, ... }:
+pkgs.python3Packages.buildPythonPackage {
+  pname = "pixelflux";
+  version = "2.0.0";
+  format = "wheel";
+
+  src = pkgs.python3Packages.fetchPypi {
+    pname = "pixelflux";
+    version = "2.0.0";
+    format = "wheel";
+    dist = "cp314";
+    python = "cp314";
+    abi = "cp314";
+    platform = "manylinux_2_28_x86_64";
+    hash = "sha256-fFAdGE+6eXRspjEYtcxGKFG3ytIXCU4RoBFpazv6OVM=";
+  };
+
+  nativeBuildInputs = [ pkgs.autoPatchelfHook ];
+
+  buildInputs = with pkgs; [
+    libva
+    libdrm
+    mesa
+    wayland
+    libxkbcommon
+    libGL
+    pixman
+    stdenv.cc.cc.lib
+  ];
+
+  pythonImportsCheck = [ "pixelflux" ];
+
+  meta.description = "Selkies pixelflux (prebuilt manylinux wheel)";
+}
